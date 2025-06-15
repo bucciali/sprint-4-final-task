931e723e819b2396134eb9616f26b86105e4a835
Danya <daniagoldin@yandex.ru>
2025-06-15 16:43:47 +0300

    second commit

--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -25,15 +25,22 @@ func parseTraining(data string) (int, string, time.Duration, error) {
 	}
 	steps, err := strconv.Atoi(sl[0])
 	// обработка ошибки с переводом в инт
-	if err != nil || steps <= 0 {
+	if err != nil {
 		return 0, "", 0, fmt.Errorf("mistake with convert int type")
+	}
+
+	if steps <= 0 {
+		return 0, "", 0, fmt.Errorf("steps less than or equal to zero")
 	}
 	typeOfActivity := sl[1]
 
 	timeSpent, err := time.ParseDuration(sl[2])
 	//обработка ошибки с переводом в тайм дуратион
-	if err != nil || timeSpent <= 0 {
+	if err != nil {
 		return 0, "", 0, fmt.Errorf("mistake with parsing time")
+	}
+	if timeSpent <= 0 {
+		return 0, "", 0, fmt.Errorf("timeSpent less than or equal to zero")
 	}
 	return steps, typeOfActivity, timeSpent, nil
 
@@ -51,7 +58,16 @@ func distance(steps int, height float64) float64 {
 
 func meanSpeed(steps int, height float64, duration time.Duration) float64 {
 	// TODO: реализовать функцию
-	if duration <= 0 || steps <= 0 || height <= 0 {
+
+	if steps <= 0 {
+		return 0
+	}
+
+	if height <= 0 {
+		return 0
+	}
+
+	if duration <= 0 {
 		return 0
 	}
 	distance := distance(steps, height)
@@ -66,13 +82,14 @@ func TrainingInfo(data string, weight, height float64) (string, error) {
 	steps, typeOfActivity, timeSpent, _ := parseTraining(data)
 	distance := distance(steps, height)
 	speed := meanSpeed(steps, height, timeSpent)
-	caloriesByWalk, _ := WalkingSpentCalories(steps, weight, height, timeSpent)
-	caloriesByRunning, _ := RunningSpentCalories(steps, weight, height, timeSpent)
+
 	switch typeOfActivity {
 	case "Бег":
+		caloriesByRunning, _ := RunningSpentCalories(steps, weight, height, timeSpent)
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", typeOfActivity, timeSpent.Hours(), distance, speed, caloriesByRunning), nil
 
 	case "Ходьба":
+		caloriesByWalk, _ := WalkingSpentCalories(steps, weight, height, timeSpent)
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", typeOfActivity, timeSpent.Hours(), distance, speed, caloriesByWalk), nil
 
 	default:
@@ -84,24 +101,20 @@ func TrainingInfo(data string, weight, height float64) (string, error) {
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
 
-	//
-	// ПРОВЕРИТЬ НА КОРРЕКТНОСТЬ ДАННЫЕ ВСЕ
-	//
-
 	if steps <= 0 {
-		return 0, fmt.Errorf("steps меньше или равен 0")
+		return 0, fmt.Errorf("steps less than or equal to zero")
 	}
 
 	if weight <= 0 {
-		return 0, fmt.Errorf("weight меньше или равен 0")
+		return 0, fmt.Errorf("weight less than or equal to zero")
 	}
 
 	if height <= 0 {
-		return 0, fmt.Errorf("height меньше или равен 0")
+		return 0, fmt.Errorf("height less than or equal to zero")
 	}
 
 	if duration <= 0 {
-		return 0, fmt.Errorf("duration меньше или равен 0")
+		return 0, fmt.Errorf("duration less than or equal to zero")
 	}
 
 	avgSpeed := meanSpeed(steps, height, duration)
@@ -115,24 +128,20 @@ func RunningSpentCalories(steps int, weight, height float64, duration time.Durat
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
-
-	//
-	// ПРОВЕРИТЬ НА КОРРЕКТНОСТЬ ДАННЫЕ ВСЕ
-	//
 	if steps <= 0 {
-		return 0, fmt.Errorf("steps меньше или равен 0")
+		return 0, fmt.Errorf("steps less than or equal to zero")
 	}
 
 	if weight <= 0 {
-		return 0, fmt.Errorf("weight меньше или равен 0")
+		return 0, fmt.Errorf("weight less than or equal to zero")
 	}
 
 	if height <= 0 {
-		return 0, fmt.Errorf("height меньше или равен 0")
+		return 0, fmt.Errorf("height less than or equal to zero")
 	}
 
 	if duration <= 0 {
-		return 0, fmt.Errorf("duration меньше или равен 0")
+		return 0, fmt.Errorf("duration less than or equal to zero")
 	}
 
 	avgSpeed := meanSpeed(steps, height, duration)
